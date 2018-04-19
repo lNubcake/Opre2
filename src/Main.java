3387f2447d69b9929faa998801763c2ffa0f0cf0
DESKTOP-5883VKF\szapa <szapa@windowslive.com>
2018-04-19 12:30:26 +0200

    getInput method implemented, doesnt really understand the project yet.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		TextProcessor(getInput());
 
+	}
+	
+	public static String getInput()
+	{
+		Scanner myScanner = new Scanner(System.in);
+		String Result = "";
+		
+		while(myScanner.hasNextLine())
+		{
+			Result += myScanner.nextLine();
+		}
+		
+		myScanner.close();
+		return Result;
+	}
+	
+	public static void/*??*/ TextProcessor(String TextToProcess)
+	{
+		// TODO implement this
 	}
 
 }
